94e38967ea416309527b99d36d3e83250199f455
dicky gunawan <dicky24tkj@gmail.com>
2018-08-20 03:15:38 +0700

    E-LibrarySMPN8
    Sistem Informasi Perpustakaan

---
+++ siswa/index.php
@@ -0,0 +1,17 @@
+<script type="text/javascript">var yourName = prompt("Selamat Datang siswa ", "Masukkan NIS untuk masuk?");</script><?php
+require_once("../Connections/taperpus.php");
+	include "head.php";
+	include "menu.php"; 
+echo "Selamat datang di Perpustakaan ";?>
+
+<img src="perpus.jpg" class="img-responsive" />
+<br />
+<footer class="footer">
+              <div class="foot-margin">
+                <p><a>Copyright &copy : 2018 		E-Library SMP Negeri 8 Muaro Jambi All Rights Reserved. </a></p>
+              </div>
+              </div>
+    </footer> 
+<?php
+include "footer.php"; 
+?>
